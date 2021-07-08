6a17def6900815cd5ec6352df2faf313d909fb7f
kavya466 <kavyasri466@gmail.com>
2021-07-08 22:44:02 +0530

    Defect fix

--- WFHTool - JavaScript/src/background.js
+++ WFHTool - JavaScript/src/background.js
@@ -1,35 +1,30 @@
-chrome.runtime.onInstalled.addListener(function () {
-	var blockedsites = []
+var blockedsites = []
 
-	chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-		const url = tab.url
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+	const url = tab.url
 
-		if (!url || !url.startsWith('http')) {
-			return
-		}
+	if (!url || !url.startsWith('http')) {
+		return
+	}
 
-		const hostname = new URL(url).hostname
+	const hostname = new URL(url).hostname
 
-		getFromStorage(function (blockedsites) {
-			blockedsites.forEach(function (blockedsite) {
-				if (
-					hostname.indexOf(blockedsite.sitename) > -1 &&
-					blockedsite.enabled
-				) {
-					console.log('Blocking activated')
-					chrome.tabs.remove(tabId)
-				}
-			})
-		})
-	})
-
-	function getFromStorage(callback) {
-		chrome.storage.sync.get(['blockedsites'], function (result) {
-			if (result && result.blockedsites) {
-				callback(result.blockedsites)
-			} else {
-				callback([])
+	getFromStorage(function (blockedsites) {
+		blockedsites.forEach(function (blockedsite) {
+			if (hostname.indexOf(blockedsite.sitename) > -1 && blockedsite.enabled) {
+				console.log('Blocking activated')
+				chrome.tabs.remove(tabId)
 			}
 		})
-	}
+	})
 })
+
+function getFromStorage(callback) {
+	chrome.storage.sync.get(['blockedsites'], function (result) {
+		if (result && result.blockedsites) {
+			callback(result.blockedsites)
+		} else {
+			callback([])
+		}
+	})
+}
