35f39558d0e21e6ec4d312dc1b95a570d52df8e0
Vishal Kichloo <31816531+kichloo@users.noreply.github.com>
2021-07-10 13:42:44 +0530

    Added code for the timer in blocker

--- Prodist/Extension/background.js
+++ Prodist/Extension/background.js
@@ -1,9 +1,24 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function (details) {
-        return {cancel: true}
-    },
-    {urls: ["*://*.quora.com/*","*://*.facebook.com/*","*://*.instagram.com/*","*://*.twitter.com/*"]},
-    ["blocking"]
-)
+const start = 9;
+const end = 18;
 
+const current= new Date(Date.now()).getHours();
 
+function display(myText) {
+    alert (myText);
+  }
+
+if(current >= start && current <= end){
+    display("Its the productive hour! The access to selected sites is now blocked");
+
+    chrome.webRequest.onBeforeRequest.addListener(
+        function (details) {
+            return {cancel: true}
+        },
+        {urls: ["*://*.quora.com/*","*://*.facebook.com/*","*://*.instagram.com/*","*://*.twitter.com/*"]},
+        ["blocking"]
+    )
+}
+
+else{
+    display("The sites can be accessed now!");
+}
